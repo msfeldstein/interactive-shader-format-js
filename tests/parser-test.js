6c7f614db28e52e8fae83ca34b043180a95a50ed
Michael Feldstein <msfeldstein@gmail.com>
2016-07-25 17:58:10 -0700

    Remove PERSISTENT_BUFFERS and enable persistent flag in passes

--- tests/parser-test.js
+++ tests/parser-test.js
@@ -6,25 +6,37 @@ function assetLoad(name) {
 }
 
 test('Infer Generator Type', function(t) {
-	var generatorSrc = assetLoad('generator.fs')
+	var src = assetLoad('generator.fs')
 	var parser = new ISFParser();
-	parser.parse(generatorSrc);
+	parser.parse(src);
 	t.equal(parser.type, 'generator')
 	t.end()
 })
 
 test('Infer Filter Type', function(t) {
-	var generatorSrc = assetLoad('image-filter.fs')
+	var src = assetLoad('image-filter.fs')
 	var parser = new ISFParser();
-	parser.parse(generatorSrc);
+	parser.parse(src);
 	t.equal(parser.type, 'filter')
 	t.end()
 })
 
 test('Infer Transition Type', function(t) {
-	var generatorSrc = assetLoad('transition.fs')
+	var src = assetLoad('transition.fs')
 	var parser = new ISFParser();
-	parser.parse(generatorSrc);
+	parser.parse(src);
 	t.equal(parser.type, 'transition')
+	t.end()
+})
+
+test('Buffers correctly marked as persistent', function(t) {
+	var src = assetLoad('with-persistent-buffers.fs')
+	var parser = new ISFParser();
+	parser.parse(src);
+	var passes = parser.passes
+	for (var i = 0; i < passes.length - 1; i++) {
+		t.equal(passes[i].persistent, true)
+	}
+	t.equal(passes[passes.length - 1].persistent, false)
 	t.end()
 })
