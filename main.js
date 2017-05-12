f53a98dc56f82ff9ea3549a9c6e57117117a24f8
Mike Feldstein <msfeldstein@gmail.com>
2017-05-12 15:21:07 -0700

    Properly transpile down to es5

--- main.js
+++
@@ -1,3 +0,0 @@
-exports.Renderer = require('./src/ISFRenderer');
-exports.Parser = require('./src/ISFParser');
-exports.Upgrader = require('./src/ISFUpgrader');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 exports.Renderer = require('./ISFRenderer');
 exports.Parser = require('./ISFParser');
 exports.Upgrader = require('./ISFUpgrader');
