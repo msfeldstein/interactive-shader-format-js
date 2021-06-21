37659cc41a9662e76df65fac5e039f6e0d67a5a8
Tim Pietrusky <timpietrusky@gmail.com>
2021-06-21 22:48:38 +0200

    test(img-pixel): Check if conversion to texture2D is correct

--- tests/parser-test.js
+++ tests/parser-test.js
@@ -78,13 +78,43 @@ test('IMG_NORM_PIXEL to VVSAMPLER_2DBYNORM', function (t) {
     'vec2(x, y)',
     'vec3(x, y, x).xy',
     'vec4(x, y, x, y).xy',
-    'vec4(x,y,x,y).xy',
   ];
 
   variableTypes.forEach((variable) => {
     const test = {
       toReplace: IMG_NORM_PIXEL(variable),
       expectedReplacement: VVSAMPLER_2DBYNORM(variable),
+      expectedIndex: -1
+    };
+
+    t.not(fragmentShader.indexOf(test.expectedReplacement), test.expectedIndex, test.toReplace);
+  });
+  
+  t.end();
+});
+
+test('IMG_PIXEL to texture2D', function (t) {
+  let src = assetLoad('img_pixel_isf.fs');
+  const parser = new ISFParser();
+  parser.parse(src);
+  const { fragmentShader } = parser;
+
+  const IMG_PIXEL = (variable) => `IMG_PIXEL(inputImage, ${variable});`;
+  const TEXTURE2D = (variable) => `texture2D(inputImage, (${variable}) / RENDERSIZE);`;
+
+  const variableTypes = [
+    'gl_FragCoord.xy',
+    'vec2(gl_FragCoord.xy)',
+    'vec2(gl_FragCoord.x, gl_FragCoord.y)',
+    'vec2(x, y)',
+    'vec3(x, y, x).xy',
+    'vec4(x, y, x, y).xy',
+  ];
+
+  variableTypes.forEach((variable) => {
+    const test = {
+      toReplace: IMG_PIXEL(variable),
+      expectedReplacement: TEXTURE2D(variable),
       expectedIndex: -1
     };
 
